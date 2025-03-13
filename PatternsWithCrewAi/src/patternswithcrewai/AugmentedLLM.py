ba3a598620197a25c7b1f6eb781783c035f48852
KingAbdulRehman <abdulrehman558866@gmail.com>
2025-03-14 02:25:38 +0500

    little fix in augmented llm flow and add readme in uv project

--- PatternsWithCrewAi/src/patternswithcrewai/AugmentedLLM.py
+++ PatternsWithCrewAi/src/patternswithcrewai/AugmentedLLM.py
@@ -16,21 +16,17 @@ class AugmentedFlow(Flow):
     def retrieval(self):
         """Handles query and retrieval of results."""
         print("Performing Retrieval for Query/Results...")
-        self.callback("R Completed")
 
     @listen(and_(llm_processing))
     def tools(self):
         """Handles calling external tools for response generation."""
         print("Calling External Tools for Processing...")
-        self.callback("T Completed")
 
     @listen(and_(llm_processing))
     def memory(self):
         """Handles memory read/write operations."""
         print("Reading/Writing Memory for Context...")
-        self.callback("M Completed")
     
-
     @listen(llm_processing)
     def output_stage(self):
         """Handles final output generation after LLM processing is complete."""
